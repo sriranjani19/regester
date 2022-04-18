bb4cc8465be4ec5be5e2413d887d483f2fb40f53
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-18 11:07:06 +0530

    push password3

---
+++ PassWordRule3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseRegProblem
+{
+    internal class PassWordRule3
+    {
+        /// <summary>
+        /// The regex pattern
+        /// UC7-Joy.
+        /// </summary>
+        public static string Regex_Pattern = "^[a-z A-Z 0-9]{8,}$";
+        public bool validatePasswordRule3(string passwordrule3)
+        {
+            return Regex.IsMatch(passwordrule3, Regex_Pattern);
+        }
+    }
+
+}
+
