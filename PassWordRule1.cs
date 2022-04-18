c91e3b65178e336e23eb7b0e533752b3b051b7ce
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-18 11:03:50 +0530

    push password

---
+++ PassWordRule1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseRegProblem
+{
+    internal class PassWordRule1
+    {
+            /// <summary>
+    /// UC5-PassWord Rule 1.
+    /// </summary>
+        public static string Regex_Pattern = "^[a-z]{8,}$";
+        public bool validatePasswordRule1(string passwordrule1)
+        {
+            return Regex.IsMatch(passwordrule1, Regex_Pattern);
+        }
+    }
+}
+
+    
+
