77ae7cb4f7c3979ecd01adccb6ee242952437e62
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-18 10:24:20 +0530

    Add files via upload

---
+++ Name.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegProblem
+{
+    public class Names
+    {
+        /// <summary>
+        /// The regex First Name
+        /// UC1-sri
+        
+        /// </summary>
+        public static String Regex_Name = "^[A-Z]{3}";
+
+
+        public bool validateName(string Name)
+        {
+            return Regex.IsMatch(Name, Regex_Name);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Systems;
+namespace UserRegProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my User Regestation Problem");
+
+            Name name = new Name();
+            Console.WriteLine("Enter your Fast Name");
+            string Name = Console.ReadLine();
+            bool match = name.validateName(Name);
+
+            if (match)
+                Console.WriteLine("Name Matched");
+            else
+                Console.WriteLine("Name are not Matche");
+
+
+
+        }
+    }
+
