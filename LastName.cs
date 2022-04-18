6927c015c486ce58ccb351892d020e4647533af4
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-04-18 11:01:00 +0530

    push last name

---
+++ LastName.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseRegProblem
+{
+    internal class LastName
+    {
+        /// <summary>
+        /// The regex last name
+        /// UC2-sri
+        /// </summary>
+        public static String Regex_LastName = "^[A-Z]{1}[a-z]{3,}$";
+
+
+        public bool validateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_LastName);
+        }
+    }
+}
+
+    
+
